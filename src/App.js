55f0e632753477d07dcd094380d87c235890a543
Pranita Rajput <pranitarajput6795@gmail.com>
2025-01-23 16:09:56 +0530

    completed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import XDictionary from "./XDictionary"; // Import the XDictionary component
+
+function App() {
+  return (
+    <div>
+      <XDictionary /> {/* Render the dictionary component */}
+    </div>
+  );
+}
+
+export default App;
